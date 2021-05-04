a8be92a7a527963a90ca5cbb4d857404b878fc1a
leconglc <leconglc@brickmate.kr>
2021-05-04 13:17:50 +0700

    Custom file package.json, Add file proxy and handle console.log when environment is prod

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+    if (environment.production) {
+      window.console.log = function() {}
+    }
+  }
 }
